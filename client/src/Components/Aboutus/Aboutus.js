e6634d79d1025408e3a6a6bed175c869021154cb
Katherine Allen <katherineallen067@gmail.com>
2020-09-28 13:30:34 -0400

    about page added

--- client/src/Components/Aboutus/Aboutus.js
+++ client/src/Components/Aboutus/Aboutus.js
@@ -9,27 +9,32 @@ import Flag from '../../styles/assets/icons/icons8-canada-48.png';
 function Aboutus(){
     return(
         <div className="bio">
-        <div className="bio__left">
-            <h1 className="bio__name">Katherine Allen</h1>
-            <span className="bio__title">Web Developer</span>
-            <span className="bio__duration">Duration 10 Days</span>
-            <div className="bio__headshot"></div>
-        </div>
-        <div>
-            <h2>Technologies Used</h2>
-            <img src={Sass} alt="sass icon" />
-            <img src={JS} alt="javascript icon" />
-            <img src={Node} alt="nodejs icon" />
-            <img src={RIcon} alt="react icon" />
-            <h2>Addtional Resources</h2>
-            <span>Canadian Institute for Health Information 
-                <img src={Flag} alt="canadian flag" />
-            </span>
-        </div>
-        <div className="bio__contact">
-            <span className="bio__contact--linkedin">www.linkedin.com/in/katherine-allen067</span>
-            <span className="bio__contact--github">https://github.com/katherineallen067</span>
-        </div>
+            <div className="bio__left">
+                <div className="bio__left--title">
+                    <div className="bio__headshot"></div>
+                    <div className="bio__left--description">
+                        <h1 className="bio__name">Katherine Allen</h1>
+                        <h2 className="bio__title">Web Developer</h2>
+                        <span className="bio__duration">Duration 10 Days</span>
+                    </div>
+                </div>
+                <div className="bio__contact">
+                    <h3 className="bio__contact--linkedin">www.linkedin.com/in/katherine-allen067</h3>
+                    <h3 className="bio__contact--github">https://github.com/katherineallen067</h3>
+                </div>
+            </div>
+            <div className="bio__tech">
+                    <h3>Built Using</h3>
+                    <div className="bio__tech--icons">
+                        <img src={Sass} alt="sass icon" />
+                        <img src={RIcon} alt="react icon" />
+                        <img src={JS} alt="javascript icon" />
+                        <img src={Node} alt="nodejs icon" />
+                    </div>
+                        <span className="bio__tech--flag">Canadian Institute for Health Information 
+                            <img src={Flag} alt="Canadian flag" />
+                        </span>
+            </div>
         </div>
     );
 }
