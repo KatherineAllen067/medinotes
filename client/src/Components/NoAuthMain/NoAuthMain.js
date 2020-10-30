e68b2e1feefdfaa691b1ffb56be386cc36e65f0e
Katherine Allen <katherineallen067@gmail.com>
2020-10-30 13:33:14 -0400

    no auth animation changed

--- client/src/Components/NoAuthMain/NoAuthMain.js
+++ client/src/Components/NoAuthMain/NoAuthMain.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../../styles/Main.scss';
+import Pencil from '../../styles/assets/icons/pencil.png';
 
 function NoAuthMain(){
 
     return(
     <div className="mainNoAuth">
+        <div>
+            <img src={Pencil} alt="cartoon pencil" className="pencil"/>
+        </div>
         <h1 className="mainNoAuth__welcome">
             Welcome Please Login
         </h1>
