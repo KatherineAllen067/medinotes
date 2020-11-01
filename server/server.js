6d239f8276fbb796ff1ea5f6779f7756d46d8255
Katherine Allen <katherineallen067@gmail.com>
2020-11-01 18:16:31 -0500

    install backend dependies for db connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+// const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const notes = require('./routes/notes.js');
